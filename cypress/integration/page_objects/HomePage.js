ee7254e6355db00b532c10dc873ac1d587177eab
Juan Pablo Realini <JuanPablo.Realini@endava.com>
2020-02-07 17:42:38 -0300

    Added PageObject pattern support to cypress-cucumber

---
+++ cypress/integration/page_objects/HomePage.js
@@ -0,0 +1,8 @@
+class HomePage {
+  
+  visit(url) {
+    cy.visit(url);
+  }
+}
+
+export default HomePage;
--- cypress/integration/step_definitions/form-select.js
+++ cypress/integration/step_definitions/form-select.js
@@ -1,9 +1,11 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import HomePage from '../page_objects/HomePage';
 
 const url = 'https://www.techlistic.com/';
+const home = new HomePage();
 
 Given('I open the sample page', () => {
-  cy.visit(url)
+  home.visit(url)
 })
 
 Then(`the title should be {string}`, (title) => {
